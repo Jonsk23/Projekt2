19d54b6915dae5ece30f9413bc9c0af295542767
MattiasHagelin <mattias1985@gmail.com>
2016-03-09 19:41:49 +0100

    MH La till _ViewImports i Views La till _ViewStart i Views La till RenderSection för script i _Layout La till AddProduct med ett formulär i Views/Admin Ändrade från SSN till SocialSecurityNo i AddUser formuläret

--- ViFemWebShopV2/src/ViFemWebShopV2/ViewModels/AddProductVM.cs
+++ ViFemWebShopV2/src/ViFemWebShopV2/ViewModels/AddProductVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,14 @@ namespace ViFemWebShopV2.ViewModels
     public class AddProductVM
     {
       //  public string ProductID { get; set; }
+
+        [Required(ErrorMessage = "The product must have a name")]
         public string Name { get; set; }
+
+        //[Required(ErrorMessage = "The product needs a quantity")]
         public int ItemsInStock { get; set; }
+
+        [Required(ErrorMessage = "The product must have a price")]
         public int Price { get; set; }
         public string Description { get; set; }
         public string Category { get; set; }
