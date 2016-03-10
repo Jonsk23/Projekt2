ac07e5d3fcef3e7990053f00ff1ecd5145c186a5
Jon Skärlina <pinkrope@hotmail.com>
2016-03-10 16:32:27 +0100

    Commitar ej fungerande Profiles.cshtml

--- ViFemWebShopV2/src/ViFemWebShopV2/ViewModels/UserProfileVM.cs
+++ ViFemWebShopV2/src/ViFemWebShopV2/ViewModels/UserProfileVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,5 +10,20 @@ namespace ViFemWebShopV2.ViewModels
     {
         // Här ska inloggade kunder se sin medlemsinfo
         // och även sin orderhistorik.
+
+            //Hämta informationen från kakan
+        
+
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Street { get; set; }
+
+        public string City { get; set; }
+
+        public string ZipCode { get; set; }
     }
 }
