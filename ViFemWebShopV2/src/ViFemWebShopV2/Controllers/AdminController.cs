b6f370d34abb5ec1ef71b4b7d94d90853d870bac
Jon Skärlina <pinkrope@hotmail.com>
2016-03-10 10:28:36 +0100

    AddUser i AdminController tillagd

--- ViFemWebShopV2/src/ViFemWebShopV2/Controllers/AdminController.cs
+++ ViFemWebShopV2/src/ViFemWebShopV2/Controllers/AdminController.cs
@@ -12,12 +12,18 @@ namespace ViFemWebShopV2.Controllers
 {
     public class AdminController : Controller
     {
+        UserAccountContext userAccountContext;
         ProductContext productContext;
+
+        public AdminController(UserAccountContext context)
+        {
+            userAccountContext = context;
+        }
         public AdminController(ProductContext context)
         {
             productContext = context;
         }
-        // GET: /<controller>/
+
         public IActionResult Index()
         {
             return View();
@@ -27,15 +33,54 @@ namespace ViFemWebShopV2.Controllers
         {
             return View();
         }
+
         [HttpPost]
         public IActionResult AddProduct(AddProductVM viewModel)
         {
             if (!ModelState.IsValid)
+            {
                 return View(viewModel);
-            DataManager dataManager = new DataManager(productContext);
-            dataManager.AddProduct(viewModel);
+            }
+
+            try
+            {
+                DataManager dataManager = new DataManager(productContext);
+                dataManager.AddProduct(viewModel);
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError(string.Empty, "Vi har tyvärr inte plats i lagret för denna produkt. Gå till styrelsen och begär att bygga ut.");
+                return View(viewModel);
+            }
             return RedirectToAction(nameof(AdminController.Index));
-            
+
+        }
+
+        public IActionResult AddUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(AddUserVM viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+
+            try
+            {
+                DataManager dataManager = new DataManager(userAccountContext);
+                dataManager.AddUser(viewModel);
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError(string.Empty, "You don't want your business.");
+                return View(viewModel);
+            }
+            return RedirectToAction(nameof(AdminController.Index));
+
         }
     }
 }
