e8f177f3a49b6d0d3f2514081376527ae52ebd62
MattiasHagelin <mattias1985@gmail.com>
2016-03-10 14:43:51 +0100

    ShopCartVM kundvagn view

---
+++ ViFemWebShopV2/src/ViFemWebShopV2/ViewModels/ShopCartVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ViFemWebShopV2.ViewModels
+{
+    public class ShopCartVM
+    {
+        public string ProductName { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
