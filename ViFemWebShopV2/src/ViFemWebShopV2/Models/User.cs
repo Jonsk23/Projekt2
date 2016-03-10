6f892337cfe7fb670dafb411a8c089e821bf60c3
Lijoen <linus.joensson.mail@gmail.com>
2016-03-10 15:57:38 +0100

    Fungerande AddUser to DB

--- ViFemWebShopV2/src/ViFemWebShopV2/Models/User.cs
+++ ViFemWebShopV2/src/ViFemWebShopV2/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace ViFemWebShopV2.Models
 {
     public class User
     {
+        [Key]
         public int ClientID { get; set; }
         public int AccountID { get; set; }
         public string UserName { get; set; }
