f2cf4cb13f75df8cd85260225daf59a8e339d45e
eriohm <ohman.erika@hotmail.com>
2016-03-11 17:50:18 +0100

    Fixat padding

--- ViFemWebShopV2/src/ViFemWebShopV2/ViewModels/AddUserVM.cs
+++ ViFemWebShopV2/src/ViFemWebShopV2/ViewModels/AddUserVM.cs
@@ -8,34 +8,34 @@ namespace ViFemWebShopV2.ViewModels
 {
     public class AddUserVM
     {
-        [Display(Name = "Organisationsnummer")]
+        [Display(Name = "Organisationsnummer ")]
         [Required(ErrorMessage = "Company Number required")]
         public string CompanyNumber { get; set; }
 
-        [Display(Name = "Företagslösenord")]
+        [Display(Name = "Företagslösenord ")]
         [Required(ErrorMessage = "Company Password required")]
         public string CompanyPassword { get; set; }
 
-        [Display(Name = "Användarnamn")]
+        [Display(Name = "Användarnamn ")]
         [Required(ErrorMessage = "Username required")]
         public string UserName { get; set; }
 
-        [Display(Name = "E-mail")]
+        [Display(Name = "E-mail ")]
         [Required(ErrorMessage = "E-mail required")]
         [EmailAddress(ErrorMessage = "E-mail is not valid")]
         public string Email { get; set; }
 
-        [Display(Name = "Förnamn (Valfritt)")]
+        [Display(Name = "Förnamn (Valfritt) ")]
         public string FirstName { get; set; }
 
-        [Display(Name = "Efternamn (Valfritt)")]
+        [Display(Name = "Efternamn (Valfritt) ")]
         public string LastName { get; set; }
 
-        [Display(Name = "Lösenord")]
+        [Display(Name = "Lösenord ")]
         [Required(ErrorMessage = "Password required")]
         public string Password { get; set; }
 
-        [Display(Name = "Gatuadress")]
+        [Display(Name = "Gatuadress ")]
         [Required(ErrorMessage = "Street required")]
         public string Street { get; set; }
 
